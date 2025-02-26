a95bc05f9438f10a2092b6474597e67465fae82d
Brad Sickles <brad.sickles@gmail.com>
2025-02-26 09:37:33 -0500

    Fixes

--- kafka.tf
+++ kafka.tf
@@ -5,7 +5,7 @@ data "ns_connection" "kafka" {
 
 locals {
   kafka_cluster_arn       = data.ns_connection.kafka.outputs.cluster_arn
-  kafka_brokers           = data.ns_connection.kafka.outputs.bootstrap_brokers
-  kafka_port              = data.ns_connection.kafka.outputs.bootstrap_brokers_port
+  kafka_brokers           = data.ns_connection.kafka.outputs.brokers
+  kafka_port              = data.ns_connection.kafka.outputs.brokers_port
   kafka_security_group_id = data.ns_connection.kafka.outputs.security_group_id
 }
