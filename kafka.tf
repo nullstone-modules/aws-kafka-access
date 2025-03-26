17d83c907838ee60a0fcf55312e903e7654bb159
Brad Sickles <brad.sickles@gmail.com>
2025-03-26 17:26:44 -0400

    Added KAFKA_TLS_ENABLED to env vars

--- kafka.tf
+++ kafka.tf
@@ -6,6 +6,7 @@ data "ns_connection" "kafka" {
 locals {
   kafka_cluster_arn       = data.ns_connection.kafka.outputs.cluster_arn
   kafka_brokers           = data.ns_connection.kafka.outputs.brokers
+  kafka_tls_enabled       = data.ns_connection.kafka.outputs.brokers_tls_enabled
   kafka_port              = data.ns_connection.kafka.outputs.brokers_port
   kafka_security_group_id = data.ns_connection.kafka.outputs.security_group_id
 }
--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,10 @@ output "env" {
     {
       name  = "KAFKA_BROKERS"
       value = join(",", local.kafka_brokers)
+    },
+    {
+      name  = "KAFKA_TLS_ENABLED",
+      value = tostring(local.kafka_tls_enabled)
     }
   ]
 }
